d1cb3a85ab2517b4757d812ccbf635b87189cccb
noviantoniov88 <noviantoniov88@gmail.com>
2022-07-25 23:41:01 +0700

    Layout dan fungsi

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+let player =[
+    'batu',
+    'kertas',
+    'gunting'
+]
+
+let comp = [
+    'batu',
+    'kertas',
+    'gunting'
+]
+
+
+const pilihBatu = () => {
+    console.log('pilih Batu');
+    hompimpa();
+}
+
+const pilihKertas = () => {
+    console.log('pilih Kertas');
+    hompimpa();
+}
+
+const pilihGunting = () => {
+    console.log('pilih Gunting');
+    hompimpa();
+}
+
+const resetGame = () => {
+    console.log('Reset Game');
+}
+
+
+const hompimpa = () => {
+    console.log('hompimpa');
+}
